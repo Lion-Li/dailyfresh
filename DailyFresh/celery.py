f1df2eb1d144660ed9d1a46584b074195b81cdd0
lixiang <121365554@qq.com>
2018-07-28 00:51:44 +0800

    注释中添加了关于celery中worker的启动命令

--- DailyFresh/celery.py
+++ DailyFresh/celery.py
@@ -16,6 +16,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+# starting the worker precess
+# celery -A DailyFresh worker -l info
 
 @app.task(bind=True)
 def debug_task(self):
