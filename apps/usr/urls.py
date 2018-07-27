15182a00b43501f6c7ccfb0ad2c1432b436fddf2
lixiang <121365554@qq.com>
2018-07-27 11:12:09 +0800

    实现了注册后发送激活邮件的功能

--- apps/usr/urls.py
+++ apps/usr/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
-from apps.usr.views import RegisterView, verify_code
+from apps.usr.views import RegisterView, ActiveView, LoginView, verify_code
 
 app_name = 'usr'
 urlpatterns = [
     re_path(r'^register/?$', RegisterView.as_view(), name='register'),  # 注册
+    re_path(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 通过邮箱激活用户
+    re_path(r'^login/?$', LoginView.as_view(), name='login'),  # 登录页面
     path('verify_code/', verify_code, name='verify_code'),  # 图片验证码生成
 ]
--- apps/usr/views.py
+++ apps/usr/views.py
@@ -11,6 +11,11 @@ from django.views.generic import View
 from apps.usr.models import User
 import re
 
+# 邮箱
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired
+from django.conf import settings
+from django.core.mail import send_mail
 
 # 显示注册页面
 def register(request):
@@ -62,6 +67,53 @@ def verify_code(request):
     return HttpResponse(buf.getvalue(), 'image/png')
 
 
+# info加密解密模块
+class Verify(object):
+    def __init__(self):
+        # 用settings中已有的秘钥来加密信息, 生成token.
+        self.serializer = Serializer(settings.SECRET_KEY, 3600)
+
+    # 加密用户信息
+    def encypt_info(self, info):
+        token = self.serializer.dumps(info)  # token为byte类型数据
+        token = token.decode('utf8')  # 编码为utf8
+        return token
+
+    # 解密用户信息,创建用户的引用.
+    def deciphering_info(self, token):
+        # 解码加密信息
+        info = self.serializer.loads(token)
+        # 取出用户的id信息
+        usr_id = info['confirm']
+        # 创建对应用户的实例
+        user = User.objects.get(id=usr_id)
+        return user
+
+
+# 登录视图,路由: /usr/login
+class LoginView(View):
+    # 登录页面
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+# 激活用户
+class ActiveView(View):
+    def get(self, request, token):
+        try:
+            # 激活实例
+            verify = Verify()
+            user = verify.deciphering_info(token)
+            # 激活用户
+            user.is_active = 1
+            # 提交到数据库
+            user.save()
+            # 返回到登录页面
+            return redirect(reverse('usr:login'))
+        except SignatureExpired as e:
+            return HttpResponse('激活链接已过期,请重新激活.')
+
+
 # 用户注册
 class RegisterView(View):
     def get(self, request):
@@ -97,10 +149,17 @@ class RegisterView(View):
             usr = User.objects.create_user(username, password, email)
             usr.is_active = 0
             usr.save()
-
-
+            # 发送激活链接
+            verify_email = Verify()
+            info = {'confirm': usr.id}
+            token = verify_email.encypt_info(info)
+            subject = '注册激活'
+            message = ''
+            sender = settings.EMAIL_FROM
+            receiver = [email]
+            html_message = '<h3>%s,天天生鲜欢迎您的到来!</h3><br><h4>点击下面链接激活您的账户.</h4><br><a href="http://127.0.0.1:800/active/%s">http://127.0.0.1:800/active/%s</a>' % (username, token, token)
+            send_mail(subject, message, sender, receiver, html_message=html_message)
         return redirect(reverse('goods:index'))
 
 
-    #   ```````````````````
 
