d95e5f248748749a60e7ba0017e68ab78d40dc33
lixiang <121365554@qq.com>
2018-07-29 09:08:03 +0800

    为celery指定选用Redis的第一个数据库.

--- DailyFresh/celery.py
+++ DailyFresh/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DailyFresh.settings')
 
-app = Celery('DailyFresh', broker='redis://127.0.0.1:6379/')
+app = Celery('DailyFresh', broker='redis://127.0.0.1:6379/1')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
