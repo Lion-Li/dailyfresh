7f9e1da31f55a6504261a38f65f0ca7009d67ea9
lixiang <121365554@qq.com>
2018-08-04 20:29:04 +0800

    实现商品首页的模板渲染.

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from apps.goods.models import GoodsType
+from apps.goods.models import GoodsType, GoodsSKU, GoodsIndexBanner
 
 # 创建管理员账户: python manage.py createsuperuser
 admin.site.register(GoodsType)  # 在后台管理注册模型
+admin.site.register(GoodsSKU)
+admin.site.register(GoodsIndexBanner)
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from apps.goods.models import *
 
 
 def index(request):
-    return render(request, 'index.html')
+    """
+        网站首页的视图函数
+        从数据库获取各类信息来渲染模板
+    """
+    goods_type = GoodsType.objects.all()
+    goods_banner = GoodsIndexBanner.objects.all().order_by('index')
+    goods_promotion = GoodsIndexPromotion.objects.all().order_by('index')
+    # # goods_index_type = GoodsIndexType.objects.all()
+    for type in goods_type:  # GoodsType
+        type.title_banner = GoodsIndexType.objects.filter(type=type, display_type=0).order_by('index')
+        type.image_banner = GoodsIndexType.objects.filter(type=type, display_type=1).order_by('index')
+    cart_count = 0  # 购物车商品数
+
+    context = {
+        'goods_type': goods_type,
+        'goods_banner': goods_banner,
+        'goods_promotion': goods_promotion,
+        'cart_count': cart_count
+    }
+    return render(request, 'index.html', context)
 
 
 # 商品详情页
